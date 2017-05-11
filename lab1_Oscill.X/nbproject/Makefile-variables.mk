713beab5f2744fd77e689d005261e5b8008b71e5
sergehould <sergehould@vaniercollege.qc.ca>
2017-05-11 16:17:47 -0400

    test

---
+++ lab1_Oscill.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=lab1_Oscill.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/lab1_Oscill.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=lab1oscill.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/lab1oscill.x.tar
